051d5d841eb155909e03916e488821c9734e1eb0
PolkanDot <insarmiftahov@yandex.ru>
2023-03-07 20:10:36 +0300

    Finished page of editing role

--- DBProject_FrontEnd/lib/routes/edit_role.dart
+++ DBProject_FrontEnd/lib/routes/edit_role.dart
@@ -1,12 +1,47 @@
 import 'package:flutter/material.dart';
+import 'package:dropdown_textfield/dropdown_textfield.dart';
 import '../callApi/delete_role_func.dart';
 import '../callApi/edit_role_func.dart';
+import '../callApi/get_actors.dart';
 import '../models/film_and_role_data.dart';
+import '../models/actor.dart';
 
-class EditRole extends StatelessWidget {
+class EditRole extends StatefulWidget {
   EditRole({Key? key}) : super(key: key);
 
-  final _formKey = GlobalKey<FormState>();
+  @override
+  State<EditRole> createState() => _EditRoleState();
+}
+
+class _EditRoleState extends State<EditRole> {
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late SingleValueDropDownController _cnt;
+  final formKey = GlobalKey<FormState>();
+  final List<DropDownValueModel> _actorsList = [];
+
+  void wrapActors() async {
+    List<Actor>? response = await getActors();
+    if (response != null) {
+      for (Actor actor in response) {
+        _actorsList.add(DropDownValueModel(name: actor.name, value: actor.idActor));
+      }
+    }
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    _cnt = SingleValueDropDownController();
+    wrapActors();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _cnt.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,7 +60,7 @@ class EditRole extends StatelessWidget {
                 // добавить к каждому сравнение с исходным значением в поле, чтобы не вызывать апи в случае если данные не изменились
                 TextFormField(
                   onChanged: (String value) => {filmAndRole.role.namePersonage = value},
-                  decoration: const InputDecoration(labelText: "Cinema name"),
+                  decoration: const InputDecoration(labelText: "Name personage"),
                   initialValue: filmAndRole.role.namePersonage,
                   validator: (value) {
                     if (value == null || value.isEmpty) {
@@ -33,6 +68,32 @@ class EditRole extends StatelessWidget {
                     }
                     return null;
                   },
+                ),
+                SingleChildScrollView(
+                  child: Form(
+                    key: formKey,
+                    child: DropDownTextField(
+                      // initialValue: "name4",
+                      readOnly: false,
+                      controller: _cnt,
+                      clearOption: true,
+                      keyboardType: TextInputType.text,
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
+                      clearIconProperty: IconProperty(color: Colors.green),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Required field";
+                        } else {
+                          return null;
+                        }
+                      },
+                      dropDownItemCount: 6,
+                      dropDownList: _actorsList,
+                      onChanged: (value) => {
+                        if (value != "") {filmAndRole.role.idActor = value.value}
+                      },
+                    ),
+                  ),
                 ),
                 OverflowBar(
                   alignment: MainAxisAlignment.end,
