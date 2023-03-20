e00304948a9cb71c6fb154b3315c86a93da7e420
PolkanDot <insarmiftahov@yandex.ru>
2023-03-20 15:17:16 +0300

    Change session_info model and appropriate method for api get_sessions_idCinema function

--- DBProject_FrontEnd/lib/models/session_info.dart
+++ DBProject_FrontEnd/lib/models/session_info.dart
@@ -5,14 +5,18 @@ SessionInfo sessionFromJson(String str) => SessionInfo.fromJson(json.decode(str)
 class SessionInfo {
   SessionInfo({
     required this.idSession,
+    required this.idCinema,
     required this.idHall,
+    required this.filmName,
     required this.hallNumber,
     required this.hallType,
     required this.dateTime,
   });
 
   int idSession;
+  int idCinema;
   int idHall;
+  String filmName;
   int hallNumber;
   int hallType;
   DateTime dateTime;
@@ -21,7 +25,9 @@ class SessionInfo {
 
   factory SessionInfo.fromJson(Map<String, dynamic> json) => SessionInfo(
     idSession: json["idSession"],
+    idCinema: json["idCinema"],
     idHall: json["idHall"],
+    filmName: json["filmName"],
     hallNumber: json["hallNumber"],
     hallType: json["hallType"],
     dateTime: DateTime.parse(json["dateTime"]),
